f91754f3ca6e565791d586aa6c8ade8eda34ca52
dmaramba <48565724+dmaramba@users.noreply.github.com>
2026-05-25 13:02:32 +0200

    Add files via upload

--- ExamsData/Examination.cs
+++ ExamsData/Examination.cs
@@ -36,5 +36,48 @@ namespace ExamsData
             }
             return marks;
         }
+
+        public void AddMark(Mark mark)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var query = "INSERT INTO marks (StudentNumber, Mark, Grade) VALUES (@studentNumber, @mark, @grade)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@studentNumber", mark.StudentNumber);
+                cmd.Parameters.AddWithValue("@mark", mark.MarkValue);
+                cmd.Parameters.AddWithValue("@grade", mark.Grade);
+                cmd.ExecuteNonQuery();// Execute the query to insert the mark into the database
+            }
+        }
+        public void UpdateMark(Mark mark)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var query = "UPDATE marks SET StudentNumber = @studentNumber,Mark = @mark, Grade = @grade WHERE Id = @id";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", mark.Id);
+                cmd.Parameters.AddWithValue("@studentNumber", mark.StudentNumber);
+                cmd.Parameters.AddWithValue("@mark", mark.MarkValue);
+                cmd.Parameters.AddWithValue("@grade", mark.Grade);
+                cmd.ExecuteNonQuery();// Execute the query to update the mark in the database
+            }
+        }
+
+        public void DeleteMark(int id)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var query = "DELETE FROM marks WHERE Id = @id";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@id", id);
+                cmd.ExecuteNonQuery();// delete the mark from the database
+            }
+        }
     }
 }
